9c89679c7f4bd4329e67c44b0ff7942e9cb69dfe
rijogeorge <rijogeorge7@gmail.com>
2016-04-14 16:11:29 -0700

    custom font added to all textviews

--- app/src/main/java/com/logicshades/tipcalculator/activities/MainActivity.java
+++ app/src/main/java/com/logicshades/tipcalculator/activities/MainActivity.java
@@ -14,7 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Typeface gothicFont = Typeface.createFromAsset(getAssets(), "Kozuka-Gothic-Pro-M_26793.ttf");
+        TextView textView_billAmount=(TextView)findViewById(R.id.textView_billAmount);
+        textView_billAmount.setTypeface(gothicFont);
+
+        TextView textView_tip=(TextView)findViewById(R.id.textView_tip);
+        textView_tip.setTypeface(gothicFont);
+
         TextView tv_billAmount=(TextView)findViewById(R.id.textView_billAmount);
         tv_billAmount.setTypeface(gothicFont);
+
+        TextView textViewtipAmount=(TextView)findViewById(R.id.textViewtipAmount);
+        textViewtipAmount.setTypeface(gothicFont);
+
+        TextView textViewsplitbill=(TextView)findViewById(R.id.textViewsplitbill);
+        textViewsplitbill.setTypeface(gothicFont);
+
+        TextView textView_splitNo=(TextView)findViewById(R.id.textView_splitNo);
+        textView_splitNo.setTypeface(gothicFont);
+
+        TextView textView_billTotal=(TextView)findViewById(R.id.textView_billTotal);
+        textView_billTotal.setTypeface(gothicFont);
+
+        TextView textView_billTotalAmount=(TextView)findViewById(R.id.textView_billTotalAmount);
+        textView_billTotalAmount.setTypeface(gothicFont);
+
     }
 }
